75abf7a512f150d590f95ef9f7c852b2494df0e6
Ricarda <ricarda.winklhofer@geogebra.at>
2021-06-09 14:38:21 +0200

    APPS-2974 unselect av button on load & set oppositve view correctly

--- web/src/main/java/org/geogebra/web/full/gui/toolbarpanel/ToolbarPanel.java
+++ web/src/main/java/org/geogebra/web/full/gui/toolbarpanel/ToolbarPanel.java
@@ -368,6 +368,9 @@ public class ToolbarPanel extends FlowPanel
 		if ((Perspective.GRAPHER_3D + "").equals(
 				app.getConfig().getForcedPerspective())) {
 			viewId = App.VIEW_EUCLIDIAN3D;
+		} else if ((Perspective.PROBABILITY + "").equals(
+				app.getConfig().getForcedPerspective())) {
+			viewId = App.VIEW_PROBABILITY_CALCULATOR;
 		}
 		DockPanelW opposite =
 				(DockPanelW) app.getGuiManager().getLayout().getDockManager().getPanel(viewId);
@@ -1375,5 +1378,9 @@ public class ToolbarPanel extends FlowPanel
 		 * Called when tab is activated.
 		 */
 		protected abstract void onActive();
+	}
+
+	public void setAVIconNonSelect(boolean exam) {
+		navRail.setAVIconNonSelect(exam);
 	}
 }
