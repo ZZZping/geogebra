bae5c056f3650016da16b796a883b62d043f5044
Jan <jan.borovsky@geogebra.at>
2021-07-14 07:00:09 +0200

    Check GeoSurfaceCartesion functions array size

--- common/src/main/java/org/geogebra/common/kernel/arithmetic/FunctionExpander.java
+++ common/src/main/java/org/geogebra/common/kernel/arithmetic/FunctionExpander.java
@@ -117,11 +117,20 @@ public class FunctionExpander implements Traversing {
 					if (en2.getLeft() instanceof GeoSurfaceCartesianND) {
 						FunctionNVar[] fun = ((GeoSurfaceCartesianND) en2
 								.getLeft()).getFunctions();
-						MyVec3DNode vect = new MyVec3DNode(
-								((ExpressionNode) ev).getKernel(),
-								fun[0].getExpression(), fun[1].getExpression(),
-								fun[2].getExpression());
-						en2 = new ExpressionNode(en.getKernel(), vect);
+						MyVecNDNode vector;
+						if (fun.length > 2) {
+							vector = new MyVec3DNode(
+									((ExpressionNode) ev).getKernel(),
+									fun[0].getExpression(),
+									fun[1].getExpression(),
+									fun[2].getExpression());
+						} else {
+							vector = new MyVecNode(
+									((ExpressionNode) ev).getKernel(),
+									fun[0].getExpression(),
+									fun[1].getExpression());
+						}
+						en2 = new ExpressionNode(en.getKernel(), vector);
 						if (en.getRight() instanceof MyList
 								&& ((MyList) en.getRight()).getListElement(
 										0) instanceof ExpressionNode
